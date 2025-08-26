bdd88ce454f1354a598a360525be0453f95e3334
levan1988 <valiko7@yahoo.com>
2025-08-26 19:39:20 +0200

    Add files via upload

---
+++ BitlockerScript.ps1
@@ -0,0 +1,4 @@
+﻿$CdriveStatus = Get-BitLockerVolume -MountPoint 'c:'
+if ($CdriveStatus.volumeStatus -eq 'FullyDecrypted') {
+    C:\Windows\System32\manage-bde.exe -on c: -recoverypassword -skiphardwaretest
+}
